3426bd71459c032f4ba8e506d253c876abad4085
Osvaldo <osvaldoceron31@gmail.com>
2017-12-14 10:02:40 -0600

    Nuevo codigo de un formulario

---
+++ form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE >
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text"/html; charset=UTF-8/>
+        <title>Formulario HTML</title>
+      
+    </head>
+    <body>
+        <form action="formulario.php" method="GET" name ="frm">
+
+		<input type="text" name="nombre" />
+        <input type="text" name="apellido" />
+         <input type="password" name="pass" />
+        <input type="submit" value="enviar" />
+        </form>
+    </body>
+</html>
